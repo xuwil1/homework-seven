2a59d536c1ae85b1f7ab415e390877803cbf9eca
xuwil1 <73079502+xuwil1@users.noreply.github.com>
2021-01-22 14:30:27 -0500

    Delete test.java

--- test.java
+++
@@ -1,9 +0,0 @@
-
-public class test {
-	public static void main(String[]args) {
-		String roadNames="Lawn City,Hipster,16";
-		String[] temp= roadNames.split(",");
-		System.out.println(temp[0]);
-	}
-
-}
